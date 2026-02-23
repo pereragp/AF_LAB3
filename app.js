e9bb8bf84e3ec1025759049b466b35eb00b92964
Tharindu Mishen <tharindumishen@gmail.com>
2026-02-23 23:45:36 +0530

    Add a change

--- app.js
+++ app.js
@@ -3,60 +3,61 @@ console.log("Hello Lab 4");
 const fs = require("fs");
 
 // Read the contents of a file
-fs.readFile('file.txt', 'utf8', function (err, data) {
+fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
 });
 
 // Write to a file
-fs.writeFile('file.txt', 'Hello World!', function (err) {
+fs.writeFile("file.txt", "Hello World!", function (err) {
   if (err) throw err;
-  console.log('File saved!');
+  console.log("File saved!");
 });
 
 //Create web server
-const http = require('http');
+const http = require("http");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-  res.end();
-}).listen(8080);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(8080);
 
 console.log("Server running at http://localhost:8080");
 
 //Making an HTTP request
-const https = require('https');
+const https = require("https");
 
-https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-  let data = '';
+https
+  .get("https://jsonplaceholder.typicode.com/posts/1", (resp) => {
+    let data = "";
 
-  resp.on('data', (chunk) => {
-    data += chunk;
+    resp.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    resp.on("end", () => {
+      console.log(JSON.parse(data));
+    });
+  })
+  .on("error", (err) => {
+    console.log("Error: " + err.message);
   });
-
-  resp.on('end', () => {
-    console.log(JSON.parse(data));
-  });
-
-}).on('error', (err) => {
-  console.log("Error: " + err.message);
-});
-
 
 //Using a module
-const myModule = require('./my-module.js');
+const myModule = require("./my-module.js");
 console.log(myModule.myFunction());
-
 
 //Promises
 const condition = true;
 
 const myPromise = new Promise((resolve, reject) => {
   if (condition) {
-    resolve('Success!');
+    resolve("Success!");
   } else {
-    reject('Failure!');
+    reject("Failure!");
   }
 });
 
@@ -68,9 +69,8 @@ myPromise
     console.log(error);
   });
 
-
-  //Using async/await
-  async function myFunction() {
+//Using async/await
+async function myFunction() {
   try {
     const result = await myPromise;
     console.log(result);
@@ -83,3 +83,4 @@ myFunction();
 
 //Collaboration chang
 console.log("Git Collaboration done by bimalka");
+console.log("Git Collaboration done by mishen");
