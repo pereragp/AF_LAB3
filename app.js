f46760656f109a39b50fab8e633fa82fed09549f
pereragp <pereragp09@gmail.com>
2026-02-23 22:02:09 +0530

    Making an HTTP request

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
   console.log('File saved!');
 });
 
+//Create web server
 const http = require('http');
 
 http.createServer(function (req, res) {
@@ -23,3 +24,21 @@ http.createServer(function (req, res) {
 }).listen(8080);
 
 console.log("Server running at http://localhost:8080");
+
+//Making an HTTP request
+const https = require('https');
+
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
+});
